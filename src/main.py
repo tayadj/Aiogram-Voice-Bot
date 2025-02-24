8d543e7e385bba1f6188482bb82397bb1d94181c
tayadj <tayadj@users.noreply.github.com>
2025-02-24 22:09:07 +0300

    Added query-search

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import asyncio
+
 import openai
+import pydantic
 import pydantic_settings
 
 
@@ -10,8 +12,8 @@ class Settings(pydantic_settings.BaseSettings):
 		env_file = '.env', env_file_encoding = 'utf-8', extra = 'ignore'
 	)
 
-	OPENAI_API_TOKEN: str
-	TELEGRAM_TOKEN: str
+	OPENAI_API_TOKEN: pydantic.SecretStr
+	TELEGRAM_TOKEN: pydantic.SecretStr
 
 settings = Settings()
 
@@ -19,6 +21,44 @@ settings = Settings()
 
 class Engine():
 
-	def __init__():
+	def __init__(self, api_key: str):
+
+		self.client = openai.AsyncOpenAI(api_key = api_key)
+
+	async def voice_to_speech(self):
 
 		pass
+
+	async def speech_to_voice(self):
+
+		pass
+
+	async def search(self, query: str) -> str:
+
+		response = await self.client.chat.completions.create(
+			model = 'gpt-3.5-turbo',
+			messages = [{'role': 'user', 'content': query}]
+		)
+		content = response.choices[0].message.content		
+
+		return content
+
+
+
+class Bot():
+
+	pass
+
+
+
+if __name__ == '__main__':
+
+	engine = Engine(settings.OPENAI_API_TOKEN.get_secret_value())
+
+	async def main():
+
+		query = "Test query: what is the capital of France?"
+		result = await engine.search(query)
+		print(result)
+
+	asyncio.run(main())
