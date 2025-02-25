283aacea7d9e1fe8214438eeca7f073a973129a7
tayadj <tayadj@users.noreply.github.com>
2025-02-25 15:04:57 +0300

    Refactored from completions API to assistant API

--- src/main.py
+++ src/main.py
@@ -25,27 +25,78 @@ class Engine():
 
 		self.client = openai.AsyncOpenAI(api_key = openai_api_token)
 
+		self.assistant = None
+		self.thread = None
+
+		self.setup_event = asyncio.Event()
+		asyncio.create_task(self.assistant_setup())
+
+	async def assistant_setup(self):
+
+		self.assistant = await self.client.beta.assistants.create(
+			model = 'gpt-3.5-turbo',
+			instructions = 'You are a search assistant.'
+		)
+		self.thread = await self.client.beta.threads.create()
+
+		self.setup_event.set()
+
 	async def voice_to_text(self, path: str) -> str:
 
 		with open(path, 'rb') as voice_file:
 
-			response = await self.client.audio.transcriptions.create(file = voice_file, model = 'whisper-1')
+			response = await self.client.audio.transcriptions.create(
+				file = voice_file, 
+				model = 'whisper-1'
+			)
 
 		return response.text
 
 	async def text_to_voice(self, text: str, path: str):
 
-		response = await self.client.audio.speech.create(input = text, voice = 'nova', model = 'tts-1')
+		response = await self.client.audio.speech.create(
+			input = text, 
+			voice = 'nova', 
+			model = 'tts-1'
+		)
 
 		response.stream_to_file(path)
 
 	async def search(self, query: str) -> str:
 
-		response = await self.client.chat.completions.create(
-			model = 'gpt-3.5-turbo',
-			messages = [{'role': 'user', 'content': query}]
+		await self.setup_event.wait()
+
+		message = await self.client.beta.threads.messages.create(
+			thread_id = self.thread.id,
+            role = 'user',
+            content = query
 		)
-		content = response.choices[0].message.content		
+
+		run = await self.client.beta.threads.runs.create(
+			thread_id = self.thread.id,
+			assistant_id = self.assistant.id
+		)
+
+		while run.status not in ['completed', 'failed']:
+
+			await asyncio.sleep(1)
+			run = await self.client.beta.threads.runs.retrieve(
+				thread_id = self.thread.id,
+				run_id = run.id
+			)
+
+		content = None
+
+		if run.status == 'completed':
+
+			messages = await self.client.beta.threads.messages.list(
+				thread_id = self.thread.id
+			)
+			content = messages.data[0].content[0].text.value if messages else 'Server Error.'	
+
+		else:
+
+			content = run.status
 
 		return content
 
@@ -110,4 +161,6 @@ if __name__ == '__main__':
 		await bot.run()
 
 	asyncio.run(main())
+
+
 	
