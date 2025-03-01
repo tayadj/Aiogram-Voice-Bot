d5ce84eb55ebd262646e276b219a881bf623495e
tayadj <tayadj@users.noreply.github.com>
2025-03-01 22:34:56 +0300

    Test execution

--- src/core/handlers/handle_profile_command.py
+++ src/core/handlers/handle_profile_command.py
@@ -4,8 +4,15 @@ import aiogram
 
 async def handle_profile_command(message: aiogram.types.Message, engine, database):
 
-	async with database.session_local() as session:
+	try:
+
+		async with database.session_local() as session:
 	
-		user = await database.model_user.read_user(session, message.from_user.id)
+			user = await database.model_user.read_user(session, message.from_user.id)
 		
-		await message.answer(f'Profile\n\nValues: {user.values}')
+			await message.answer(f'Profile\n\nValues: {user.values}')
+
+	except Exception as exception:
+
+		print(exception)
+		await message.answer('Oops! Something is wrong.')
